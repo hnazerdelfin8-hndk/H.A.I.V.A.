ddf45cb177109e10e73dc3cca0a4973b136a69a5
Sixhundredgoons <hnazerdelfin8@gmail.com>
2026-08-30 12:19:39 +0800

    Set up H.A.I.V.A. application loader
    Initialize H.A.I.V.A. application and handle errors.

--- core/app.js
+++ core/app.js
@@ -1,1 +1,18 @@
+// H.A.I.V.A. Application Loader
 
+console.log("H.A.I.V.A. is starting...");
+
+const app = document.getElementById("haiva-app");
+
+if (!app) {
+  throw new Error("H.A.I.V.A. application root not found.");
+}
+
+app.innerHTML = `
+  <div>
+    <h1>H.A.I.V.A.</h1>
+    <p>System initializing...</p>
+  </div>
+`;
+
+console.log("H.A.I.V.A. core loaded.");
