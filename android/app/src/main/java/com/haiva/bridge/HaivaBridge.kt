5c9b4cc61b3bc078996c6c46c373a0b8eba2f05b
Sixhundredgoons <hnazerdelfin8@gmail.com>
2026-09-08 20:33:30 +0800

    V3: add native TTS cancellation bridge

--- android/app/src/main/java/com/haiva/bridge/HaivaBridge.kt
+++ android/app/src/main/java/com/haiva/bridge/HaivaBridge.kt
@@ -4,5 +4,6 @@ interface HaivaBridge {
     fun startVoiceCapture()
     fun stopVoiceCapture()
     fun speak(text: String)
+    fun stopSpeaking()
     fun notify(title: String, message: String)
 }
