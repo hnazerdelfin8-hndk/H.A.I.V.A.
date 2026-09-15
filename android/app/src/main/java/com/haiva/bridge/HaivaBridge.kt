804f27f0b132ab65df8ad4640bef84398395d0a8
Sixhundredgoons <hnazerdelfin8@gmail.com>
2026-09-15 09:25:45 +0800

    V3 dedicated capture: add native V3 bridge channel

--- android/app/src/main/java/com/haiva/bridge/HaivaBridge.kt
+++ android/app/src/main/java/com/haiva/bridge/HaivaBridge.kt
@@ -3,6 +3,8 @@ package com.haiva.bridge
 interface HaivaBridge {
     fun startVoiceCapture()
     fun stopVoiceCapture()
+    fun startV3VoiceCapture()
+    fun stopV3VoiceCapture()
     fun speak(text: String)
     fun stopSpeaking()
     fun notify(title: String, message: String)
