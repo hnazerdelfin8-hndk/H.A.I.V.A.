108dfca4904093dece5ca7141bb9f476a520268f
Sixhundredgoons <hnazerdelfin8@gmail.com>
2026-09-17 09:54:05 +0800

    feat(android): expose duplex interrupt monitor bridge

--- android/app/src/main/java/com/haiva/bridge/HaivaBridge.kt
+++ android/app/src/main/java/com/haiva/bridge/HaivaBridge.kt
@@ -5,6 +5,8 @@ interface HaivaBridge {
     fun stopVoiceCapture()
     fun startV3VoiceCapture()
     fun stopV3VoiceCapture()
+    fun startDuplexInterruptMonitor(turn: Long)
+    fun stopDuplexInterruptMonitor()
     fun speak(text: String)
     fun stopSpeaking()
     fun notify(title: String, message: String)
