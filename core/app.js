b671cb3fab4d35561b2a3456d89a14e2eda04295
Sixhundredgoons <hnazerdelfin8@gmail.com>
2026-09-18 16:40:35 +0800

    voice: switch app to canonical interaction entrypoint

--- core/app.js
+++ core/app.js
@@ -8,7 +8,7 @@ import { CONFIG } from "./config.js";
 import { HAIVAAssistant } from "./assistant.js";
 import { setUIState, setVoiceButtonActive, hasNativeVoiceBridge } from "./ui-bridge.js";
 import { bootCheckpoint } from "./boot-diagnostics.js";
-import { VoiceInteraction } from "./voice/interaction/index.js";
+import { VoiceInteraction } from "./voice/interaction.js";
 
 bootCheckpoint("JS_ENTRY_STARTED", "core/app.js module evaluated");
 
